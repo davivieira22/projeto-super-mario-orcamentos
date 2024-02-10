e6502fc0e1064a28ef5599f6dff2e86dad087fc3
davivieira22 <nftdavidavila22@gmail.com>
2024-02-09 21:26:57 -0300

    projeto super mario

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".fale-conosco")
+const mascara = document.querySelector(".mascara-form")
+function mostraform() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+} function esconderform() {
+    form.style.left = "-330px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+}
